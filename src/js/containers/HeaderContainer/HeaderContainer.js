51302f0e94ee0fc497953058895322f6e5973647
Denis Melnikov <denmel02@gmail.com>
2019-02-28 20:54:51 +0400

    Роутинг

--- src/js/containers/HeaderContainer/HeaderContainer.js
+++ src/js/containers/HeaderContainer/HeaderContainer.js
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {push} from 'connected-react-router';
 import {KINDS_OF_TABLE} from '../../constants';
 import {headerContainerSelector} from '../../selectors';
 import {changeKindOfTable, logout} from '../../actions';
@@ -10,17 +11,28 @@ export class HeaderContainer extends PureComponent {
     propTypes = {
         kindOfTable: PropTypes.oneOf(KINDS_OF_TABLE).isRequired,
         changeKindOfTable: PropTypes.func.isRequired,
-        logout: PropTypes.func.isRequired
+        logout: PropTypes.func.isRequired,
+        push: PropTypes.func.isRequired
     };
 
     render = () => {
-        const {kindOfTable, changeKindOfTable, logout} = this.props;
+        const {kindOfTable, changeKindOfTable} = this.props;
 
         return (
-            <HeaderComponent kindOfTable={kindOfTable} onChangeKindOfTable={changeKindOfTable} onLogout={logout} />
+            <HeaderComponent
+                kindOfTable={kindOfTable}
+                onChangeKindOfTable={changeKindOfTable}
+                onLogout={this.handleLogout}
+            />
         );
+    };
+
+    handleLogout = () => {
+        const {logout, push} = this.props;
+
+        logout().then(() => push('/login'));
     };
 }
 
-export default connect(headerContainerSelector(), {changeKindOfTable, logout})(HeaderContainer);
+export default connect(headerContainerSelector(), {changeKindOfTable, logout, push})(HeaderContainer);
 
--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
+import {connectRouter} from 'connected-react-router';
 import intl from './intl';
 import settings from './settings';
 import tasks from './tasks';
 import user from './user';
 
-export default combineReducers({
+export const createReducers = (history) => combineReducers({
+    router: connectRouter(history),
     intl,
     settings,
     tasks,
